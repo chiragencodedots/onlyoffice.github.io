3ea2875f576c9950a5ffbf125e06610fe8e84e3b
Chirag M <chirag.encodedots@gmail.com>
2023-10-03 23:40:30 +0530

    Update code.js

--- sdkjs-plugins/content/propact_dev/scripts/code.js
+++ sdkjs-plugins/content/propact_dev/scripts/code.js
@@ -466,6 +466,11 @@
 
             // Clause Lazyload functionality
             document.getElementById('contractListItemsDiv').onscroll = async function (e) {
+                if (document.getElementById('contractListItemsDiv').scrollHeight > document.getElementById('contractListItemsDiv').scrollTop + document.getElementById('contractListItemsDiv').offsetHeight) {
+                    document.getElementById('scrollDownBtn').classList.remove(displayNoneClass);
+                } else {
+                    document.getElementById('scrollDownBtn').classList.add(displayNoneClass);
+                }
                 if (document.getElementById('contractListItemsDiv').scrollTop + document.getElementById('contractListItemsDiv').offsetHeight >= (document.getElementById('contractListItemsDiv').scrollHeight - 1)) {
                     if (clauseHasNextPage) {
                         await getContractSectionList();
