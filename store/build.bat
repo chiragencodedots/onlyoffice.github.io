b1422c4caa5a8088244f32317db93a0cf2a46f38
Chirag <chirag.encodedots@gmail.com>
2024-03-08 11:30:19 +0530

    Resolved JS error

--- store/build.bat
+++ store/build.bat
@@ -3,6 +3,6 @@ echo package installation
 call npm install
 echo package installation end
 echo start compilation
-java -jar ./node_modules/google-closure-compiler-java/compiler.jar --js ./store/scripts/code.js --js_output_file ./store/scripts/code_min.js
+java -jar ./node_modules/google-closure-compiler-java/compiler.jar --js ./store/scripts/code-bkp.js --js_output_file ./store/scripts/code_min.js
 echo end compilation
 pause
