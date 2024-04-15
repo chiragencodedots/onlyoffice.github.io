818c30852f4fcf4b19986a395a531f6754de2349
Chirag <chirag.encodedots@gmail.com>
2024-04-15 09:45:46 +0530

    Show/Hide Clause button based on contract status

--- sdkjs-plugins/content/propact/scripts/plugin.js
+++ sdkjs-plugins/content/propact/scripts/plugin.js
@@ -2582,6 +2582,7 @@
                         contractInformation = responseData.openContractDetails;
                         loggedInUserDetails = responseData.loggedInUserDetails;
                         loggedInCompanyDetails = responseData.loggedInCompanyDetails;
+                        switchClass(elements.btnCreateClause, displayNoneClass, (contractInformation && contractInformation.contractCurrentStatus != "Under Negotiation"));
                         if (contractInformation.counterPartyInviteStatus !== 'Pending') {
                             counterPartyDetail = responseData.oppositeUser;
                         }
